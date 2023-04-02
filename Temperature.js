df4e85bc3ad1cf459af92b386dffa8d089882350
prabhukori0 <128037834+prabhukori0@users.noreply.github.com>
2023-04-02 16:57:27 +0530

    Update Temperature.js

--- Temperature.js
+++ Temperature.js
@@ -4,7 +4,7 @@ cel.addEventListener("input", function () {
     // let c = this.value;
     // let fahren = (c * 9/5) + 32;
     let fahren= (this.value * 9/5) + 32;
-    if(Number.isInteger(fahren)){
+    if(!Number.isInteger(fahren)){
         fahren = fahren.toFixed(3);
     }
     fah.value = fahren;            
@@ -14,7 +14,7 @@ fah.addEventListener("input", function (){
     // let f = this.value;
     // let celsi = (f − 32) × 5/9;
     let celsi = (this.value-32)*5/9;
-    if(Number.isInteger(celsi)){
+    if(!Number.isInteger(celsi)){
         celsi = celsi.toFixed(3);
     }
     cel.value = celsi;
